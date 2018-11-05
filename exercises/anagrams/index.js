ae50687978eee6a83a2a19d1459b8379b0c4a808
Ryan Kent <imsolost@gmail.com>
2018-11-05 15:22:18 -0800

    update solution for anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -10,39 +10,12 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  let aCharMap = buildCharMap(stringA)
-  let bCharMap = buildCharMap(stringB)
-
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false
-  }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char] ) {
-      return false
-    }
-  }
-
-  return true
-}
-
-function buildCharMap(str) {
-  const charMap = {}
-
-  for (let char of str.replace(/[^\w]/g,'').toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1
-  }
-
-  return charMap
+  let sortedA = stringA.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
+  let sortedB = stringB.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
+  return sortedA === sortedB
 }
 
 module.exports = anagrams;
-
-// function anagrams(stringA, stringB) {
-//   let sortedA = stringA.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
-//   let sortedB = stringB.replace(/[^\w]/g,'').toLowerCase().split('').sort().join('')
-//   return sortedA === sortedB
-// }
 
 // function anagrams(stringA, stringB) {
 //   const helper = (str) => str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
