7daad148fc596da7ab921be14287746af0e2b40f
Ryan Kent <imsolost@gmail.com>
2018-11-04 21:40:50 -0800

    maxchar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {}
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return Object.keys(charMap).reduce((a, b) => charMap[a] > charMap[b] ? a : b)
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//   let max = 0
+//   let maxChar = ''
+//
+//   let charMap = {}
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+//
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char]
+//       maxChar = char
+//     }
+//   }
+//
+//   return maxChar
+// }
