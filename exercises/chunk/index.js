1cfb351139fc8cc07e3638e8595c18b98f45e673
Ryan Kent <imsolost@gmail.com>
2018-11-05 10:56:00 -0800

    update new solution to chunked

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,20 +10,30 @@
 
 function chunk(array, size) {
   let chunked = []
-
-  for (let element of array) {
-    const last = chunked[chunked.length - 1]
-
-    if (!last || last.length === size) {
-      chunked.push([element])
-    } else {
-      last.push(element)
-    }
+  let index = 0
+  while (index < array.length) {
+      chunked.push(array.slice(index, index + slice))
+      index += size
   }
   return chunked
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let chunked = []
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1]
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//   return chunked
+// }
 
 // function chunk(array, size) {
 //   let cont = []
