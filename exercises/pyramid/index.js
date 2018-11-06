200931de2230482099074b0cabdebb48ff29a9d0
Ryan Kent <imsolost@gmail.com>
2018-11-05 20:16:26 -0800

    pyramid iterative solution complete

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,45 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2)
+
+  for (let row = 0; row < n; row++) {
+    let level = ''
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) {
+        level += '#'
+      } else {
+        level += ' '
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let array = Array(2 * n - 1).fill(' ')
+//     console.log( array.fill('#', n - i, n + i - 1).join('') )
+//   }
+// }
+
+// function pyramid(n) {
+//   const mid = Math.floor((2 * n - 1) / 2)
+//
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+//
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (mid - row <= col && mid + row >= col) {
+//         level += '#'
+//       } else {
+//         level += ' '
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
